71421c01d1f41c21285c005488ad354a91190176
Andryushkov Konstantin <actimind-kandryushkov@evernote.com>
2021-02-07 19:07:39 +0300

    help-1 add counter && online indicators

--- src/main-block/avatars/AvatarUser/AvatarUser.jsx
+++ src/main-block/avatars/AvatarUser/AvatarUser.jsx
@@ -1,46 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import s from './AvatarUser.module.css'
 import avatar from "../../images/avatar.png";
 import status from "../../images/status.png";
 import pencil from "../../images/pencil.png";
 import offline from "../../images/offline.png";
 
-let sumArray = [0, 0, 0, 0, 0]
 
-let Random = (min, max) => {
-    let rand = min - 0.5 + Math.random() * (max - min + 1);
-    return Math.round(rand);
-}
+const AvatarUser = ({messageCounter, resetCounter, isForceOffline, resetOffline}) => {
+    const [isOnline, setIsOnline] = useState(false);
 
-let messageCounter =() => {
-        if (Random(1, 5) === 1) {
-        sumArray[0] += 1
-    } else if (Random(1, 5) === 2) {
-        sumArray[1] += 1
-    } else if (Random(1, 5) === 3) {
-        sumArray[2] += 1
-    } else if (Random(1, 5) === 4) {
-        sumArray[3] += 1
-    }
-    sumArray[4] = sumArray[0] + sumArray[1] + sumArray[2] + sumArray[3];
-}
-
-
-
-
-const AvatarUser = (props) => {
     return (
-        <div className={`${s.userContainer} + ${s.user1}`}>
-            <div className={s.counter}></div>
+        <div className={`${s.userContainer} + ${s.user1}`} onClick={() => {
+            resetCounter();
+            resetOffline();
+            setIsOnline(true);
+        }}>
+            <div className={`${s.counter} ${messageCounter && s.visible}`}>{messageCounter}</div>
             <div className={s.status}>
                 <img src={avatar} alt='lul'/>
-                <div className={`${s.onlineWrites} + ${s.online1}`}>
+                <div className={`${s.onlineWrites} ${s.online1} ${isOnline && !messageCounter && !isForceOffline &&  s.visible}`}>
                     <img src={status} alt="lul"/>
                 </div>
-                <div className={`${s.onlineWrites} + ${s.writes1}`}>
+                <div className={`${s.onlineWrites} ${s.writes1} ${messageCounter && !isForceOffline && s.visible}`}>
                     <img src={pencil} alt="lul"/>
                 </div>
-                <div className={`${s.offline} + ${s.offline1}`}>
+                <div className={`${s.offline} ${s.offline1} ${isForceOffline && s.visible}`}>
                     <img src={offline} alt='lul'/>
                 </div>
             </div>
--- src/main-block/avatars/Avatars.jsx
+++ src/main-block/avatars/Avatars.jsx
@@ -1,12 +1,74 @@
-import React from "react"
+import React, {useState, useCallback, useEffect} from "react"
 import s from './Avatars.module.css'
 import circle from '../images/circle.png'
 import nouser from '../images/no_user.png'
 import messagecounter from '../images/message_counter.svg'
 import AvatarUser from "./AvatarUser/AvatarUser";
 
+let Random = (min, max) => {
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
+}
+
 
 const Avatars = (props) => {
+  const [totalMessageCounter, setTotalMessageCounter] = useState(0);
+
+  const [messageCounter1, setMessageCounter1] = useState(0);
+  const [messageCounter2, setMessageCounter2] = useState(0);
+  const [messageCounter3, setMessageCounter3] = useState(0);
+  const [messageCounter4, setMessageCounter4] = useState(0);
+
+  const [isForceOffline1, setIsForceOffline1] = useState(false);
+  const [isForceOffline2, setIsForceOffline2] = useState(false);
+  const [isForceOffline3, setIsForceOffline3] = useState(false);
+  const [isForceOffline4, setIsForceOffline4] = useState(false);
+
+
+  useEffect(() => {
+    setInterval(() => {
+      setMessageCounter1(prevCount => prevCount + 1)
+    }, 2000);
+
+    setInterval(() => {
+      setMessageCounter2(prevCount => prevCount + 1)
+    }, 4000);
+
+    setInterval(() => {
+      setMessageCounter3(prevCount => prevCount + 1)
+    }, 5000);
+
+    setInterval(() => {
+      setMessageCounter4(prevCount => prevCount + 1)
+    }, 6000)
+  }, []);
+
+  useEffect(() => {
+    setInterval(() => {
+      setIsForceOffline1(true)
+    }, 10000);
+
+    setInterval(() => {
+      setIsForceOffline2(true)
+    }, 20000);
+
+    setInterval(() => {
+      setIsForceOffline3(true)
+    }, 25000);
+
+    setInterval(() => {
+      setIsForceOffline4(true)
+    }, 30000)
+  }, []);
+
+  useEffect(() => {
+
+  }, []);
+
+  useEffect(() => {
+    setTotalMessageCounter(messageCounter1 + messageCounter2 + messageCounter3 + messageCounter4);
+  }, [messageCounter1, messageCounter2, messageCounter3, messageCounter4]);
+
     return (
         <div className={s.container}>
             <div className={s.userContainer}>
@@ -17,14 +79,29 @@ const Avatars = (props) => {
                 </div>
             </div>
 
-            <AvatarUser sum=''/>
-            <AvatarUser sum=''/>
-            <AvatarUser sum=''/>
-            <AvatarUser sum=''/>
+            <AvatarUser resetCounter={() => {setMessageCounter1(0)}}
+                        resetOffline={() => {setIsForceOffline1(false)}}
+                        isForceOffline={isForceOffline1}
+                        messageCounter={messageCounter1}/>
+
+            <AvatarUser resetCounter={() => {setMessageCounter2(0)}}
+                        resetOffline={() => {setIsForceOffline2(false)}}
+                        isForceOffline={isForceOffline2}
+                        messageCounter={messageCounter2} />
+
+            <AvatarUser resetCounter={() => {setMessageCounter3(0)}}
+                        resetOffline={() => {setIsForceOffline3(false)}}
+                        isForceOffline={isForceOffline3}
+                        messageCounter={messageCounter3} />
+
+            <AvatarUser resetCounter={() => {setMessageCounter4(0)}}
+                        resetOffline={() => {setIsForceOffline4(false)}}
+                        isForceOffline={isForceOffline4}
+                        messageCounter={messageCounter4} />
 
             <div className={`${s.userContainer} + ${s.user5}`}>
                 <img src={messagecounter} alt='lul'/>
-                <div className={`${s.counter} + ${s.messagesSum5}`}></div>
+                <div className={`${s.counter} + ${s.messagesSum5} ${totalMessageCounter && s.visible}`}>{totalMessageCounter}</div>
                 <div className="avatars__message-icon"></div>
             </div>
         </div>);
